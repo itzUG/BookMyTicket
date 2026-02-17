1b6a4ad947742c2e2b01de38d5a9ce00f1118071
Utkarsh <utakrshhgupta24@gmail.com>
2026-02-18 01:57:34 +0530

    ..

--- app/src/main/java/com/ticket/bookMyTicket/MainActivity.kt
+++ app/src/main/java/com/ticket/bookMyTicket/MainActivity.kt
@@ -21,7 +21,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovieBookingApp()
+            MovieBookingApp(
+                //Text
+            )
         }
     }
 }
